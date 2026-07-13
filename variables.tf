a1b7186caf8290af229aec7ecd409065ae4aa4b3
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:41:13 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ EOT
     location            = string
     name                = string
     resource_group_name = string
-    size_gb             = optional(number) # Default: 100
+    size_gb             = optional(number)
     tags                = optional(map(string))
     identity = object({
       identity_ids = optional(set(string))
